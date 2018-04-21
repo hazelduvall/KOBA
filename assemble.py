25add84034d067cda22abab638fec3c5e6a10d83
Keegan Lanzillotta <swfanboy01@gmail.com>
2018-04-21 00:28:21 -0400

    Make UX Slower, but cooler

--- assemble.py
+++ assemble.py
@@ -1,5 +1,7 @@
 import sys, time
 
+print("\033c")
+
 def error(text = ""):
 	if text != "":
 		print("Error: " + text)
@@ -33,12 +35,47 @@ if len(sys.argv) > 3:
 
 infile = sys.argv[1]
 
-print("Welcome to the Kind Of Bad Assembler")
+def terminal_size():
+    import fcntl, termios, struct
+    h, w, hp, wp = struct.unpack('HHHH',
+        fcntl.ioctl(0, termios.TIOCGWINSZ,
+        struct.pack('HHHH', 0, 0, 0, 0)))
+    return w, h
+
+time.sleep(0.5)
+
+if terminal_size()[0] < 60:
+	print("Welcom to the: \n KIND OF BAD ASSEMBLER")
+else:
+	print((((terminal_size()[0] - 8) // 2) * " ") + "Welcome to the: \n" + 
+		(((terminal_size()[0] - 58) // 2) * " ") + "╦╔═┬┌┐┌┌┬┐  ╔═╗┌─┐  ╔╗ ┌─┐┌┬┐  ╔═╗┌─┐┌─┐┌─┐┌┬┐┌┐ ┬  ┌─┐┬─┐\n" + 
+		(((terminal_size()[0] - 58) // 2) * " ") + "╠╩╗││││ ││  ║ ║├┤   ╠╩╗├─┤ ││  ╠═╣└─┐└─┐├┤ │││├┴┐│  ├┤ ├┬┘\n" + 
+		(((terminal_size()[0] - 58) // 2) * " ") + "╩ ╩┴┘└┘─┴┘  ╚═╝└    ╚═╝┴ ┴─┴┘  ╩ ╩└─┘└─┘└─┘┴ ┴└─┘┴─┘└─┘┴└─\n\n")
+time.sleep(0.5)
+
 print("You've selected the following options")
-print("     Verbose Mode: " + ("◉  ON" if verbose else "◎  OFF"))
-print("   Overwrite Mode: " + ("◉  ON" if force else "◎  OFF"))
-print("     Quantum Mode: " + ("◉  ON" if quantum else "◎  OFF"))
-print("Reading file " + infile + "...")
+time.sleep(0.2)
+print("     Verbose Mode: ", end="", flush=True)
+time.sleep(0.5)
+print("◉  ON" if verbose else "◎  OFF")
+time.sleep(0.2)
+print("   Overwrite Mode: ", end="", flush=True)
+time.sleep(0.5)
+print("◉  ON" if force else "◎  OFF")
+time.sleep(0.2)
+print("     Quantum Mode: ", end="", flush=True)
+time.sleep(0.5)
+print("◉  ON" if quantum else "◎  OFF")
+time.sleep(0.2)
+
+
+print("Reading file " + infile, end=" ", flush=True)
+time.sleep(0.5)
+print(".", end="", flush=True)
+time.sleep(0.5)
+print(".", end="", flush=True)
+time.sleep(0.8)
+print(".")
 
 try:
 	with open(infile) as file:
@@ -47,16 +84,14 @@ try:
 except FileNotFoundError:
 	error(text = "No file " + infile + " found")
 
+if len(lines) == 0:
+	error(text = "File " + infile + " is empty")
+
+time.sleep(0.5)
+
 print("Done.\n")
 
 print("Assembling file...")
-
-def terminal_size():
-    import fcntl, termios, struct
-    h, w, hp, wp = struct.unpack('HHHH',
-        fcntl.ioctl(0, termios.TIOCGWINSZ,
-        struct.pack('HHHH', 0, 0, 0, 0)))
-    return w, h
 
 def progress_bar(lines):
 	line = 0
@@ -72,6 +107,8 @@ def progress_bar(lines):
 bar = progress_bar(len(lines))
 for line in lines:
 	next(bar)  #progress bar, prettty
+	if quantum:
+		time.sleep(0.5)
 
 
 # cleanup
