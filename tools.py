e9b36a27e21e70920a42c9d2a9777bbbbaab0f44
hazelduvall <2019hduvall@tjhsst.edu>
2018-04-23 21:59:16 -0400

    Add basic code to parse one line
    not much so far, but is pretty good I think

--- tools.py
+++ tools.py
@@ -1,52 +1,81 @@
-hex = {
-	"0" : 0b0000,
-	"1" : 0b0001,
-	"2" : 0b0010,
-	"3" : 0b0011,
-	"4" : 0b0100,
-	"5" : 0b0101,
-	"6" : 0b0110,
-	"7" : 0b0111,
-	"8" : 0b1000,
-	"9" : 0b1001,
-   	"A" : 0b1010,
-	"B" : 0b1011,
-	"C" : 0b1100,
-	"D" : 0b1101,
-	"E" : 0b1110,
-	"F" : 0b1111,
-}
+from instructions import instructions
+import re
 
 def parse_literal(lit):
-	if lit[:2] == "0x":
-		return (hex[lit[2]] << 4 | hex[lit[3]]) & 0xFF
-	if lit[0] == "@":
-		return ((hex[lit[1]] & 0b111) << 6 | (hex[lit[2]] & 0b111) << 3 | hex[lit[3]]) & 0xFF
-	if lit[0] == "+":
-		return int(lit[1:]) & 0xFF
-	if lit[0] == "-":
-		return ((1 << 8) - int(lit[1:])) & 0xFF
-	if lit[0] == "0":
-		return 0
-	if lit[0] == "b":
-		return int(lit[1:], 2) & 0xFF
-	if lit == "*":
-		return 0b00000000
+    value = 0
+    if lit[:2] == "0x":
+        value = int(lit[2:], 16)
+    elif lit[0] == "@":
+        value = int(lit[1:], 8)
+    elif lit[0] == "+":
+        value = int(lit[1:], 10)
+    elif lit[0] == "-":
+        value = ((1 << 8) - int(lit[1:], 10))
+    elif lit[0] == "0":
+        value = 0
+    elif lit[0] == "b":
+        value = int(lit[1:], 2)
+    elif lit == "*":
+        value = 0b00000000
+    else:
+        return None
+
+    return value & 0xFF
+
+line_regex = re.compile("(?P<opcode>[a-zA-Z_\.]+)\[(?P<subcode>[a-zA-Z_\.0-9]+)\]\s*(?P<data>[0-9a-fA-F\-\+@x]+)?")
+
+def parse_line(line):
+    # Strip all leading and trailing whitespace
+    line = line.strip()
+    match = re.match(line_regex, line)
+
+    if match is None:
+        # The line is not valid
+        # TODO: non-silent error handling
+        return []
+    
+    instr = instructions.get(match.group("opcode"), None)
+    if instr is None:
+        # There is not an instruction with that opcode
+        # Also do error handling here
+        return []
+
+    # Fancy trick to return 0 if the bit mapping is empty, but None if the subcode doesn't exist
+    sub = len(instr.bit_mapping) and instr.bit_mapping.get(match.group("subcode"), None)
+    if sub is None:
+        # Also a malformed instruction
+        # need to do error handling like before
+        return []
+
+    first_part = (instr.bit_mask & instr.bit_id) | (sub & (0xFF ^ instr.bit_id))
+
+    data = match.group("data")
+    
+    if data:
+        second_part = parse_literal(data)
+        if second_part is None:
+            # Malformed data
+            # error handling
+            return []
+        return [first_part, second_part]
+    else:
+        return [first_part]
+    
 
 
 if __name__ == "__main__":
-	print("     0xFF = " + format(parse_literal("0xFF"), '08b'))
-	print("     0x1F = " + format(parse_literal("0x1F"), '08b'))
-	print("     @131 = " + format(parse_literal("@131"), '08b'))
-	print("       +8 = " + format(parse_literal("+8"), '08b'))
-	print("       +1 = " + format(parse_literal("+1"), '08b'))
-	print("       +0 = " + format(parse_literal("+0"), '08b'))
-	print("        0 = " + format(parse_literal("0"), '08b'))
-	print("       -0 = " + format(parse_literal("-0"), '08b'))
-	print("       -1 = " + format(parse_literal("-1"), '08b'))
-	print("       -6 = " + format(parse_literal("-6"), '08b'))
-	print("     -128 = " + format(parse_literal("-128"), '08b'))
-	print("     +127 = " + format(parse_literal("+127"), '08b'))
-	print("     +128 = " + format(parse_literal("+128"), '08b'))
-	print("      b01 = " + format(parse_literal("b01"), '08b'))
-	print("b01111111 = " + format(parse_literal("b01111111"), '08b'))
+    print("     0xFF = " + format(parse_literal("0xFF"), '08b'))
+    print("     0x1F = " + format(parse_literal("0x1F"), '08b'))
+    print("     @131 = " + format(parse_literal("@131"), '08b'))
+    print("       +8 = " + format(parse_literal("+8"), '08b'))
+    print("       +1 = " + format(parse_literal("+1"), '08b'))
+    print("       +0 = " + format(parse_literal("+0"), '08b'))
+    print("        0 = " + format(parse_literal("0"), '08b'))
+    print("       -0 = " + format(parse_literal("-0"), '08b'))
+    print("       -1 = " + format(parse_literal("-1"), '08b'))
+    print("       -6 = " + format(parse_literal("-6"), '08b'))
+    print("     -128 = " + format(parse_literal("-128"), '08b'))
+    print("     +127 = " + format(parse_literal("+127"), '08b'))
+    print("     +128 = " + format(parse_literal("+128"), '08b'))
+    print("      b01 = " + format(parse_literal("b01"), '08b'))
+    print("b01111111 = " + format(parse_literal("b01111111"), '08b'))
